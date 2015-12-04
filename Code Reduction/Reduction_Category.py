4f518cc830fc6b7ed5af9ce6a6d20ac58c044c3f
TRIVED MENTA <trived.menta@ufl.edu>
2015-12-04 13:28:09 -0500

    Category Reduction Added

--- Code Reduction/Reduction_Category.py
+++ Code Reduction/Reduction_Category.py
@@ -2,9 +2,9 @@
 from datetime import datetime
 import os
 path = os.getcwd()
-loc_offers = path+'/offers'
-loc_transactions = path+'/transactions'
-loc_reduced = path+'/reduced_company' # will be created
+loc_offers = path+'/offers.csv'
+loc_transactions = path+'/transactions.csv'
+loc_reduced = path+'/reduced_category' # will be created
 
 def reduce_data(loc_offers, loc_transactions, loc_reduced):
 
@@ -12,7 +12,7 @@ def reduce_data(loc_offers, loc_transactions, loc_reduced):
   #get all categories on offer in a dict
   offers = {}
   for e, line in enumerate( open(loc_offers) ):
-    offers[ line.split(",")[3] ] = 1
+    offers[ line.split(",")[1] ] = 1
   #open output file
   with open(loc_reduced, "wb") as outfile:
     #go through transactions file and reduce
@@ -21,8 +21,8 @@ def reduce_data(loc_offers, loc_transactions, loc_reduced):
       if e == 0:
         outfile.write( line ) #print header
       else:
-        #only write when category in offers dict
-          if line.split(",")[4] in offers:
+        #only write when Company is in offers dict
+          if line.split(",")[3] in offers:
             outfile.write( line )
             reduced += 1
       #progress
